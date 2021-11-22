c8e6e106b2d5ef06e6044da00ed7155a6adcad60
Mylene <pont.mylene@gmail.com>
2021-11-22 11:09:50 +0900

    AJAX

--- javascript/main.js
+++ javascript/main.js
@@ -7,7 +7,32 @@ document.querySelector(".js-go").addEventListener('click', function(){
     pushToDom(input);
 });
 
+
+document.querySelector(".js-userinput").addEventListener('keyup', function(e){
+    
+    // alert("test");
+    console.log(e);
+   var input = document.querySelector("input").value;
+    console.log(input);
+
+    if(e.which===13) {
+        pushToDom(input);
+    }
+});
+
 // Do the data stuff with the API
+
+var url = "http://api.giphy.com/v1/gifs/search?q=brooklyn+99&api_key=5WHGVBiL6dgD2R7ebfAOkMC0nE08dlk6"
+
+// Ajax request
+var GiphyAJAXCall = new XMLHttpRequest();
+GiphyAJAXCall.open( 'GET', url);
+GiphyAJAXCall.send();
+
+GiphyAJAXCall.addEventListener( 'load', function(e) {
+    var data = e.target.response;
+    console.log(data);
+})
 
 // Show me the GIFs
 function pushToDom(input) {
